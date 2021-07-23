59d85989bb0e9d40a955a2600f18b680268da688
João Vitor Medeiros <joaoolimedeiros@gmail.com>
2021-07-23 19:58:14 -0300

    navigation-2

--- main.js
+++ main.js
@@ -37,3 +37,35 @@ window.addEventListener('scroll', function () {
 
     }
 })
+
+// Testimonials carousel slider - swiper
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    //mousewheel: true,
+    keyboard: true
+});
+
+// ScrollReveal: mostrar elementos quando der scroll na página
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `
+    #home .text, #home .image, 
+     #about .text,#about .image,
+     #services .card, #services header, 
+     #testimonials .testimonials, #testimonials header,
+     #about .links, #contact .text 
+
+    `,
+    { interval: 100 }
+)
